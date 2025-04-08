60365ebd4685031f196f184d10ab8c69a9fe6435
James-Lian <166276178+James-Lian@users.noreply.github.com>
2025-04-07 22:03:23 -0400

    Add files via upload

--- api/trackUsage.js
+++ api/trackUsage.js
@@ -13,7 +13,7 @@ module.exports = async (req, res) => {
     // if (allowedOrigins.includes(origin)) {
     //     res.setHeader('Access-Control-Allow-Origin', origin); // Dynamically allow specific origins
 
-    const redis = Redis.fromEnv()
+    // const redis = Redis.fromEnv()
 
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -23,73 +23,74 @@ module.exports = async (req, res) => {
         res.status(200).end();
         return;
     }
-
-    const keyCount = "api_usage_count";
-    const keyMonth = "last_accessed_month";
-    const currentMonth = new Date().getUTCMonth();
-
-    const lastMonth = await redis.get(keyMonth);
-
-    const { dictType, reqType, reqWord } = req.body;
-    
-    let collinsResponse;
-    let collinsData;
-    let apiUsage;
-
-    // if the month has changed, thus requiring a counter reset
-    if (!lastMonth || parseInt(lastMonth) !== currentMonth) {
-        await redis.set(keyCount, 1);
-        await redis.set(keyMonth, currentMonth);
-
-    } else {
-        apiUsage = await redis.incr(keyCount);
-    }
-    
-    if (apiUsage <= 4800) {
-        try {
-            if (reqType == "best-matching") {
-                let URL = `${hostname}/api/v1/dictionaries/${dictType}/search/first/?q=${reqWord}`;
-                collinsResponse = await fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        'accessKey': accessKey,
-                        'Accept': 'application/json'
-                    }
-                });
-            } else if (reqType == "get-entry") {
-                let URL = `${hostname}/api/v1/dictionaries/${dictType}/entries/${reqWord}`;
-                collinsResponse = await fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        'accessKey': accessKey,
-                        'Accept': 'application/json'
-                    }
-                })
-            } else if (reqType == "did-you-mean") {
-                let URL = `${hostname}/api/v1/dictionaries/${dictType}/search/didyoumean/?q=${reqWord}&entrynumber=${numSearchResults}`;
-                collinsResponse = await fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        'accessKey': accessKey,
-                        'Accept': 'application/json'
-                    }
-                })
-            } else if (reqType == "make-a-search") {
-                let URL = `${hostname}/api/v1/dictionaries/${dictType}/search/?q=${reqWord}&pagesize=${numSearchResults}`;
-                collinsResponse = await fetch(URL, {
-                    method: 'GET',
-                    headers: {
-                        'accessKey': accessKey,
-                        'Accept': 'application/json'
-                    }
-                })
-            }
-            collinsData = await collinsResponse.json()
-            res.status(200).json({ message: "Collins API call succeeded", data: collinsData, apiCallCount: apiUsage })
-        } catch {
-            res.status(500).json({ error: "Internal Server Error." })
-        }
-    } else {
-        res.status(200).json({ message: "Exceeded Collins API usage count for the month. ", apiCallCount: apiUsage })
-    }
+    res.status(200).json({"message": "f"})
 }
+//     const keyCount = "api_usage_count";
+//     const keyMonth = "last_accessed_month";
+//     const currentMonth = new Date().getUTCMonth();
+
+//     const lastMonth = await redis.get(keyMonth);
+
+//     const { dictType, reqType, reqWord } = req.body;
+    
+//     let collinsResponse;
+//     let collinsData;
+//     let apiUsage;
+
+//     // if the month has changed, thus requiring a counter reset
+//     if (!lastMonth || parseInt(lastMonth) !== currentMonth) {
+//         await redis.set(keyCount, 1);
+//         await redis.set(keyMonth, currentMonth);
+
+//     } else {
+//         apiUsage = await redis.incr(keyCount);
+//     }
+    
+//     if (apiUsage <= 4800) {
+//         try {
+//             if (reqType == "best-matching") {
+//                 let URL = `${hostname}/api/v1/dictionaries/${dictType}/search/first/?q=${reqWord}`;
+//                 collinsResponse = await fetch(URL, {
+//                     method: 'GET',
+//                     headers: {
+//                         'accessKey': accessKey,
+//                         'Accept': 'application/json'
+//                     }
+//                 });
+//             } else if (reqType == "get-entry") {
+//                 let URL = `${hostname}/api/v1/dictionaries/${dictType}/entries/${reqWord}`;
+//                 collinsResponse = await fetch(URL, {
+//                     method: 'GET',
+//                     headers: {
+//                         'accessKey': accessKey,
+//                         'Accept': 'application/json'
+//                     }
+//                 })
+//             } else if (reqType == "did-you-mean") {
+//                 let URL = `${hostname}/api/v1/dictionaries/${dictType}/search/didyoumean/?q=${reqWord}&entrynumber=${numSearchResults}`;
+//                 collinsResponse = await fetch(URL, {
+//                     method: 'GET',
+//                     headers: {
+//                         'accessKey': accessKey,
+//                         'Accept': 'application/json'
+//                     }
+//                 })
+//             } else if (reqType == "make-a-search") {
+//                 let URL = `${hostname}/api/v1/dictionaries/${dictType}/search/?q=${reqWord}&pagesize=${numSearchResults}`;
+//                 collinsResponse = await fetch(URL, {
+//                     method: 'GET',
+//                     headers: {
+//                         'accessKey': accessKey,
+//                         'Accept': 'application/json'
+//                     }
+//                 })
+//             }
+//             collinsData = await collinsResponse.json()
+//             res.status(200).json({ message: "Collins API call succeeded", data: collinsData, apiCallCount: apiUsage })
+//         } catch {
+//             res.status(500).json({ error: "Internal Server Error." })
+//         }
+//     } else {
+//         res.status(200).json({ message: "Exceeded Collins API usage count for the month. ", apiCallCount: apiUsage })
+//     }
+// }
