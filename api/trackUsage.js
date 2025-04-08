077b9677d3605b34570264d702fa3c49a99cda44
James-Lian <166276178+James-Lian@users.noreply.github.com>
2025-04-07 20:21:35 -0400

    Add files via upload

--- api/trackUsage.js
+++ api/trackUsage.js
@@ -16,6 +16,11 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     const keyCount = "api_usage_count";
     const keyMonth = "last_accessed_month";
     const currentMonth = new Date().getUTCMonth();
