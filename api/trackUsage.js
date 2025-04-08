0009d77cf571518387de60a650dce327187523d5
James-Lian <166276178+James-Lian@users.noreply.github.com>
2025-04-07 20:40:14 -0400

    Add files via upload

--- api/trackUsage.js
+++ api/trackUsage.js
@@ -12,7 +12,12 @@ const numSearchResults = 5;
 
 export default async function handler(req, res) {
     // Add CORS headers to allow requests from the client-side
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500'); 
+    const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500']; // Adjust port as per Live Server
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin); // Dynamically allow specific origins
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
