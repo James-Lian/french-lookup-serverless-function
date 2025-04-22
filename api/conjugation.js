faa08d9b2ecba3193017af1215af8815f5aca8ab
James-Lian <166276178+James-Lian@users.noreply.github.com>
2025-04-22 00:56:41 -0400

    Add files via upload

--- api/conjugation.js
+++ api/conjugation.js
@@ -2,12 +2,12 @@ import conjugationFr from 'conjugation-fr';
 
 module.exports = async (req, res) => {
     // Add CORS headers to allow requests from the client-side
-    const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500']; // Adjust port as per Live Server
-    const origin = req.headers.origin;
+    // const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5500']; // Adjust port as per Live Server
+    // const origin = req.headers.origin;
 
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin); // Dynamically allow specific origins
-    }
+    // if (allowedOrigins.includes(origin)) {
+    //     res.setHeader('Access-Control-Allow-Origin', origin); // Dynamically allow specific origins
+    // }
 
     // res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
